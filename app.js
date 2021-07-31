92141307d05240c28cbcce82f6e44b458719dbfb
celia22 <celialopez22@gmail.com>
2021-07-31 12:43:05 +0200

    delee cookie parser

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
@@ -29,7 +28,6 @@ async function setupApp() {
 	app.use(logger('dev'));
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: false }));
-	app.use(cookieParser());
 	app.use(express.static(path.join(__dirname, 'public')));
 	app.set('trust proxy', 1);
 
